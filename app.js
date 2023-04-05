2790749e177ccc1e8045aee65dd91edbd885b56c
Seth Broweleit <seth.broweleit@insight.com>
2023-04-05 13:51:14 -0400

    dialed in

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 let hue = "0deg";
-let bri = "1";
+let bri = "100%";
+let sat = "100%";
+let con = "100%";
+let lgImage = document.getElementById("lgImage");
 
 function hueFunction(val) {
   hue = val + "deg";
   setIamageStyle();
 }
-
 function brightFunction(val) {
-  bri = val / 100 + "";
+  bri = val + "%";
   setIamageStyle();
 }
-
+function satFunction(val) {
+  sat = val + "%";
+  setIamageStyle();
+}
+function contFunction(val) {
+  con = val + "%";
+  setIamageStyle();
+}
 function setIamageStyle() {
-  document
-    .getElementById("lgImage")
-    .setAttribute(
-      "style",
-      "filter:hue-rotate(" + hue + ") brightness(" + bri + ")"
-    );
+  let style = "";
+  let filter = "filter:";
+  if (hue !== "0deg") {
+    filter = filter + ` hue-rotate(${hue})`;
+  }
+  if (bri !== "100%") {
+    filter = filter + ` brightness(${bri})`;
+  }
+  if (sat !== "100%") {
+    filter = filter + ` saturate(${sat})`;
+  }
+  if (con !== "100%") {
+    filter = filter + ` contrast(${con})`;
+  }
+  if (filter !== "filter:") {
+    style = filter;
+  }
+  lgImage.setAttribute("style", style);
 }
-(() => {
-  setIamageStyle();
-})();
+// setIamageStyle();
