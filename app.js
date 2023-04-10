f3402474a7bb83e72ff6f18ae3b6e46e3699a248
Seth Broweleit <seth.broweleit@insight.com>
2023-04-10 15:59:31 -0400

    v2

--- app.js
+++ app.js
@@ -1,68 +1,105 @@
-let colorPicker;
-let box1;
-let lgImage;
-let hue = "0deg";
+const box1 = document.querySelector("#box1");
+const bri_val = document.querySelector("#bri_val");
+const changed = document.querySelector("#changed");
+const code = document.querySelector("#code");
+const color_picker1 = document.querySelector("#color_picker1");
+const color_picker2 = document.querySelector("#color_picker2");
+const con_val = document.querySelector("#con_val");
+const hue_val = document.querySelector("#hue_val");
+const inp_bri = document.querySelector("#inp_bri");
+const inp_con = document.querySelector("#inp_con");
+const inp_hue = document.querySelector("#inp_hue");
+const inp_inv = document.querySelector("#inp_inv");
+const inp_sat = document.querySelector("#inp_sat");
+const sat_val = document.querySelector("#sat_val");
+const btn_reset_hue = document.querySelector("#btn_reset_hue");
+
 let bri = "100%";
-let sat = "100%";
 let con = "100%";
+let hue = "0deg";
 let inv = "0";
+let sat = "100%";
 
-function startup() {
-  lgImage = document.getElementById("lgImage");
-  colorPicker = document.querySelector("#color-picker");
-  box1 = document.querySelector("#box1");
+const colorPicked1 = (e) => {
+  box0.style.backgroundColor = e.target.value;
+};
+color_picker1.addEventListener("input", colorPicked1, false);
+const colorPicked2 = (e) => {
+  box1.style.backgroundColor = e.target.value;
+};
+color_picker2.addEventListener("input", colorPicked2, false);
 
-  colorPicker.addEventListener("input", updateFirst, false);
-  colorPicker.addEventListener("change", updateAll, false);
-}
-function hueFunction(val) {
-  hue = val + "deg";
-  setIamageStyle();
-}
-function brightFunction(val) {
-  bri = val + "%";
-  setIamageStyle();
-}
-function satFunction(val) {
-  sat = val + "%";
-  setIamageStyle();
-}
-function conFunction(val) {
-  con = val + "%";
-  setIamageStyle();
-}
-function invFunction(val) {
-  inv = val;
-  setIamageStyle();
-}
-function setIamageStyle() {
+const setImgStyle = () => {
   let style = "";
   let filter = "filter:";
-  if (hue !== "0deg") {
-    filter = filter + ` hue-rotate(${hue})`;
-  }
   if (bri !== "100%") {
-    filter = filter + ` brightness(${bri})`;
-  }
-  if (sat !== "100%") {
-    filter = filter + ` saturate(${sat})`;
+    filter += ` brightness(${bri})`;
   }
   if (con !== "100%") {
-    filter = filter + ` contrast(${con})`;
+    filter += ` contrast(${con})`;
   }
-  filter = filter + ` invert(${inv})`;
+  if (hue !== "0deg") {
+    filter += ` hue-rotate(${hue})`;
+  }
+  if (inv !== "0") {
+    filter += ` invert(${inv})`;
+  }
+  if (sat !== "100%") {
+    filter += ` saturate(${sat})`;
+  }
   style = filter;
-  lgImage.setAttribute("style", style);
-}
+  changed.setAttribute("style", style);
+  code.innerHTML = style;
+};
 
-function updateFirst(event) {
-  box1.style.backgroundColor = event.target.value;
-}
-function updateAll(event) {
-  const p = document.querySelector("p");
-  if (p) {
-    p.style.color = event.target.value;
-  }
-}
+const briFn = (e) => {
+  bri = e.target.value + "%";
+  bri_val.innerHTML = bri;
+  setImgStyle();
+};
+const conFn = (e) => {
+  con = e.target.value + "%";
+  con_val.innerHTML = con;
+  setImgStyle();
+};
+const hueFn = (e) => {
+  hue = e.target.value + "deg";
+  hue_val.innerHTML = hue;
+  setImgStyle();
+};
+const invFn = (e) => {
+  inv = e.target.value;
+  setImgStyle();
+};
+const satFn = (e) => {
+  sat = e.target.value + "%";
+  sat_val.innerHTML = sat;
+  setImgStyle();
+};
 
-window.addEventListener("load", startup, false);
+inp_bri.addEventListener("input", briFn, false);
+inp_con.addEventListener("input", conFn, false);
+inp_hue.addEventListener("input", hueFn, false);
+inp_inv.addEventListener("input", invFn, false);
+inp_sat.addEventListener("input", satFn, false);
+
+const briReset = (e) => {
+  inp_bri.value = "100";
+  briFn({ target: { value: "100" } });
+};
+btn_reset_bri.addEventListener("click", briReset, false);
+const conReset = (e) => {
+  inp_con.value = "100";
+  conFn({ target: { value: "100" } });
+};
+btn_reset_con.addEventListener("click", conReset, false);
+const hueReset = (e) => {
+  inp_hue.value = "0";
+  hueFn({ target: { value: "0" } });
+};
+btn_reset_hue.addEventListener("click", hueReset, false);
+const satReset = (e) => {
+  inp_sat.value = "100";
+  satFn({ target: { value: "100" } });
+};
+btn_reset_sat.addEventListener("click", satReset, false);
